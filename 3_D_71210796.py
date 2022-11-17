9d8b38ec2e209f353a7ed38eebdfb43b9827d982
karenjw563 <93313863+karenjw563@users.noreply.github.com>
2022-11-17 16:22:46 +0700

    Add files via upload

---
+++ 3_D_71210796.py
@@ -0,0 +1,12 @@
+#input
+N = int(input("Masukkan nama Anda : "))
+MK = int(input("Masukkan mata kuliah : "))
+G = int (input("Masukkan grup : "))
+
+p = N.MK.G.split(" ")
+d = p[0]
+t = p[0,5]
+b = p[1]
+
+#output
+print("Haloo!", b, )
